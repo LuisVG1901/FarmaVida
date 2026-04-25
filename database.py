038c21080dd65a62c587198ac4d00fdaab64b95b
Luis Vergara <vergaragarcesl@gmail.com>
2026-04-25 00:25:21 -0500

    Fix conexion base de datos Railway

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import os
 
 def conectar():
     conexion = mysql.connector.connect(
-        host=os.environ.get("MYSQLHOST"),
-        user=os.environ.get("MYSQLUSER"),
-        password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE"),
+        host=os.environ.get("mysql.railway.internal"),
+        user=os.environ.get("root"),
+        password=os.environ.get("uiFcrNEPnoOXCrSLPDoHSjYbSpEhxdiT"),
+        database=os.environ.get("railway"),
         port=int(os.environ.get("MYSQLPORT", 3306))
     )
     return conexion
